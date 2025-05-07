2f6b130e44de82eef46d6f5612b064e3d7ac03d0
laurihut <huttulajo@gmail.com>
2025-05-07 14:43:28 +0300

    styling changes

--- src/components/FloatingInfoButton.jsx
+++ src/components/FloatingInfoButton.jsx
@@ -4,10 +4,7 @@ import './FloatingInfoButton.css'; // We'll create this CSS file next
 const FloatingInfoButton = ({ onClick }) => {
   return (
     <button className="floating-info-btn" onClick={onClick} aria-label="Lisätietoja">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24px" height="24px">
-        <path d="M0 0h24v24H0V0z" fill="none"/>
-        <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
-      </svg>
+      <img src="/it.svg" alt="Info" width="24" height="24" />
     </button>
   );
 };
